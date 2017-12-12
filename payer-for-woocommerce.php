c7dfb4b5a27d8b5c51008e6264e4a719218a7779
Michael Bengtsson <michael@krokedil.se>
2017-12-12 22:48:05 +0100

    Possible fix for post code error after get address

--- payer-for-woocommerce.php
+++ payer-for-woocommerce.php
@@ -44,6 +44,7 @@ if ( ! class_exists( 'Payer_For_Woocommerce' ) ) {
 			add_filter( 'woocommerce_process_checkout_field_billing_postcode', array( $this, 'filter_pre_checked_value' ) );
 			add_filter( 'woocommerce_process_checkout_field_billing_city', array( $this, 'filter_pre_checked_value' ) );
 			add_filter( 'woocommerce_process_checkout_field_billing_company', array( $this, 'filter_pre_checked_value' ) );
+			add_filter( 'woocommerce_default_address_fields', array( $this, 'override_checkout_check' ) );
 		}
 
 		public function payer_make_purchase() {
@@ -139,16 +140,12 @@ if ( ! class_exists( 'Payer_For_Woocommerce' ) ) {
 		}
 
 		public function filter_pre_checked_value( $value ) {
-			error_log('filter_pre_checked_value');
 			// Only do this for Payer methods
 			$chosen_payment_method = WC()->session->get( 'chosen_payment_method' );
 			if ( strpos( $chosen_payment_method, 'payer' ) !== false ) {
 				$current_filter = current_filter();
-				$current_field  = str_replace( array(
-					'woocommerce_process_checkout_field_billing_',
-					'woocommerce_process_checkout_field_shipping_'
-				), '', $current_filter );
-				if ( strpos($value, '**') !== false ) {
+				$current_field  = str_replace( array( 'woocommerce_process_checkout_field_billing_', ), '', $current_filter );
+				if ( strpos( $value, '**' ) !== false ) {
 					$customer_details = WC()->session->get( 'payer_customer_details' );
 					if ( isset( $customer_details[ $current_field ] ) && '' != $customer_details[ $current_field ] ) {
 						return $customer_details[ $current_field ];
@@ -160,6 +157,14 @@ if ( ! class_exists( 'Payer_For_Woocommerce' ) ) {
 				}
 			}
 			return $value;
+		}
+
+		public function override_checkout_check( $address_fields_array ) {
+			$chosen_payment_method = WC()->session->chosen_payment_method;
+			if ( strpos( $chosen_payment_method, 'payer' ) !== false ) {
+				unset( $address_fields_array['postcode']['validate'] );
+			}
+			return $address_fields_array;			
 		}
 	}
 	new Payer_For_Woocommerce();
