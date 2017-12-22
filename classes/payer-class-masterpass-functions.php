1feda709f1fff3a955b2e44fcfe05d8f0c4e4118
Michael Bengtsson <michael@krokedil.se>
2017-12-22 09:20:33 +0100

    Added URL to MasterPass

--- classes/payer-class-masterpass-functions.php
+++ classes/payer-class-masterpass-functions.php
@@ -14,6 +14,7 @@ class Payer_Masterpass_Functions {
         $payer_masterpass_settings = get_option( 'woocommerce_payer_masterpass_settings' ); 
         if ( 'yes' === $payer_masterpass_settings['instant_masterpass_checkout'] ) {
             echo '<object type="image/svg" class="payer_instant_checkout" id="payer_instant_checkout"><img class="payer_instant_checkout" src="https://static.masterpass.com/dyn/img/btn/global/mp_chk_btn_147x034px.svg" alt="MasterPass"/> </object>';
+            echo '<a href="#" rel="external" onclick="window.open(\'http://www.mastercard.com/mc_us/wallet/learnmore/se\', \'_blank\', \'width=650,height=750,scrollbars=yes\'); return false;"><small>'. __( 'Read more...', 'payer-for-woocommerce' ) .'</small></a>';
         }
     }
     
@@ -26,6 +27,7 @@ class Payer_Masterpass_Functions {
         $payer_masterpass_settings = get_option( 'woocommerce_payer_masterpass_settings' ); 
         if ( 'yes' === $payer_masterpass_settings['instant_masterpass_checkout'] ) {
             echo '<object type="image/svg" class="payer_instant_checkout" id="payer_instant_checkout_' . $id . '"><img class="payer_instant_checkout" data-product_id="' . $id . '" src="https://static.masterpass.com/dyn/img/btn/global/mp_chk_btn_147x034px.svg" alt="MasterPass"/> </object>';
+            echo '<a href="#" rel="external" onclick="window.open(\'http://www.mastercard.com/mc_us/wallet/learnmore/se\', \'_blank\', \'width=650,height=750,scrollbars=yes\'); return false;"><small>'. __( 'Read more...', 'payer-for-woocommerce' ) .'</small></a>';            
         }
     }
 }
